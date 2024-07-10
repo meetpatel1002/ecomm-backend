cc184f580c6959f579173aca9997df7b29f04ba4
meetpatel1002 <meetspatel92@gmail.com>
2024-07-10 16:03:52 +0530

    sixth commit

--- src/routes/dbConnect.js
+++ src/routes/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose")
 
 const dbConnect = async () =>{
     try {
-        let data = await mongoose.connect("mongodb://localhost:27017/angularEcom")
+        let data = await mongoose.connect("mongodb+srv://MEET:M@cluster0.sycpeol.mongodb.net/")
     } catch (error) {
         console.log(error)
         process.exit()
