ccce2725b254b81e1cdfecfec53c0bdf718a8770
Meet <meetspatel92@gmail.com>
2025-05-28 17:13:56 +0530

    frontend url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 dbConnect();
 
 app.use(cors({
-    origin: "http://localhost:4200", // Change to your frontend URL
+    origin: "https://ecom-frontend-chi-bay.vercel.app/", // Change to your frontend URL
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true
 }));
