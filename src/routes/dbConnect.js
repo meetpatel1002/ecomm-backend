42e5db8d4b0bee2ca745bb6926e4e156cfa2272a
Meet <meetspatel92@gmail.com>
2025-05-28 17:11:37 +0530

    mongo url

--- src/routes/dbConnect.js
+++ src/routes/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose")
 
 const dbConnect = async () =>{
     try {
-        let data = await mongoose.connect("mongodb+srv://Meet:m@cluster0.bdrhd77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        let data = await mongoose.connect("mongodb+srv://Meet:M@cluster0.bdrhd77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     } catch (error) {
         console.log(error)
         process.exit()
